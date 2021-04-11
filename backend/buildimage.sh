d4c807f7dccbd8e314e69b50949995a678990f09
Stefan Matting <stefan.matting@gmail.com>
2021-04-11 19:25:43 +0200

    use image_id

--- backend/buildimage.sh
+++ backend/buildimage.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 image=$(docker load <$(nix-build -A image) | tail -n 1 | sed -n 's/.*Loaded image: \(.*\)/\1/p')
-docker tag "$image" srv:latest
+docker inspect --format='{{index .Id}}' "$image"
